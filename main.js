fbf7254a9478bedacd23423f2ddd7230d136375e
Eniola Johnson <writingincodes@gmail.com>
2020-12-08 00:38:58 +0100

    added a function skeleton

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 var $modal = document.querySelector('.modal');
 var $addEntryButton = document.querySelector('.add-entry-button');
-var $bodyBackground=document.querySelector("body")
+var $bodyBackground=document.querySelector("body");
+var $days = document.querySelectorAll('.day');
+console.log($days);
+
+function scheduledEventsDay(event){
+  
+  // var h2 = document.createElement('h2');
+  // for (var i = 0; i < $days.length; i++){
+  //   if ()
+  // }
+}
+
+
 
 document.addEventListener('click', function (event) {
   if (event.target === $addEntryButton) {
