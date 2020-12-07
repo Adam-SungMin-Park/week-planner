6238499b7c39c2eec5374eda87083a448d74c834
Daniel Sungwon Lee <danlee333@gmail.com>
2020-12-07 13:04:41 -0800

    dd

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var $modal = document.querySelector('.modal');
 var $addEntryButton = document.querySelector('.add-entry-button');
+var $bodyBackground=document.querySelector("body")
 
 document.addEventListener('click', function (event) {
   if (event.target === $addEntryButton) {
-    $modal.className = 'modal';
+    $modal.className = 'modal white-background';
+    $bodyBackground.className="gray-background"
   }
 
 });
